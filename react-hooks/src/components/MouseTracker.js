9a06869a43741d19f745331d3b4a557dd40e2851
kcshan <cshan.kong@gmail.com>
2020-12-14 21:35:21 +0800

    first commit

--- react-hooks/src/components/MouseTracker.js
+++ react-hooks/src/components/MouseTracker.js
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from 'react'
 
 const MouseTracker = () => {
   const [positions, setPositions] = useState({x: 0, y: 0})
+  useEffect(() => {
+    document.addEventListener('click', (event) => {
+      setPositions({ x: event.clientX, y: event.clientY })
+    })
+  })
   return (
   <p>X: {positions.x}, Y: {positions.y}</p>
   )
