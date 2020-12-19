087b846cab64e0d8940175eb11c12ad32c842c6f
kcshan <cshan.kong@gmail.com>
2020-12-19 23:43:04 +0800

    fix

---
+++ cloud-doc/main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron')
+const isDev = require('electron-is-dev')
+
+let mainWindow
+
+app.on('ready', () => {
+  require('devtron').install()
+  
+  mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 680,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  const urlLocation = isDev ? 'http://localhost:3000': 'dummyurl'
+  mainWindow.loadURL(urlLocation)
+})
--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,8 +1,37 @@
+import React, { useState } from 'react'
 import LikeButton from './components/LikeButton'
 import MouseTracker from './components/MouseTracker'
 import DogShow from './components/DogShow'
 import useMousePosition from './hooks/useMousePosition'
+import useURLLoader from './hooks/useURLLoader'
 import './App.css';
+
+const DogShowWithHook = () => {
+  const [data, loading] = useURLLoader('https://dog.ceo/api/breeds/image/random')
+  const style = {
+    width: 200
+  }
+  return (
+    <>
+      {loading ? <p>狗狗-读取中</p> : <img src={data && data.message} alt="dog" style={style} />}
+    </>
+  )
+}
+
+const CatShowWithHook = () => {
+  const [category, setCategory] = useState('1')
+  const [data, loading] = useURLLoader(`https://api.thecatapi.com/v1/images/search?limit=1&category_ids=${category}`)
+  const style = {
+    width: 200
+  }
+  return (
+    <>
+      {loading ? <p>猫猫-读取中</p> : <img src={data && data[0].url} alt="cat" style={style} />}
+      <button onClick={() => setCategory('1')}>帽子</button>
+      <button onClick={() => setCategory('5')}>盒子</button>
+    </>
+  )
+}
 
 function App() {
   const positions = useMousePosition()
@@ -12,7 +41,10 @@ function App() {
       <MouseTracker />
       <LikeButton />
       <br/>
-      <DogShow />
+      {/* <DogShow /> */}
+      <DogShowWithHook />
+      <br/>
+      <CatShowWithHook />
     </div>
   );
 }
