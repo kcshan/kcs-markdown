4904629f179f124a1ca83da4b629719646c4a23c
kcshan <cshan.kong@gmail.com>
2021-01-02 21:09:05 +0800

    fix

--- cloud-doc/src/App.js
+++ cloud-doc/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import FileSearch from './components/FileSearch'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container-fluid">
+      <div className="row">
+        <div className="col left-panel">
+          <FileSearch
+            onFileSearch={(value) => { console.log(value) }}
+          />
+        </div>
+        <div className="col bg-primary right-panel">
+          <h1>this is the right</h1>
+        </div>
+      </div>
     </div>
   );
 }
