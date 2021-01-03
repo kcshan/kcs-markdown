0cf32f71afdb08b0abfff1da9445ff4463c8a18d
kcshan <cshan.kong@gmail.com>
2021-01-03 17:36:29 +0800

    fix

--- cloud-doc/src/App.js
+++ cloud-doc/src/App.js
@@ -1,15 +1,42 @@
+import React from 'react'
+import { faPlus, faFileImport } from '@fortawesome/free-solid-svg-icons'
 import FileSearch from './components/FileSearch'
+import FileList from './components/FileList'
+import BottomBtn from './components/BottomBtn'
+import defaultFiles from './utils/defaultFiles'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App container-fluid">
-      <div className="row">
+    <div className="App container-fluid px-0">
+      <div className="row no-gutters">
         <div className="col left-panel">
           <FileSearch
             onFileSearch={(value) => { console.log(value) }}
           />
+          <FileList 
+            files={defaultFiles}
+            onFileClick={(id) => { console.log(id) }}
+            onFileDelete={(id) => { console.log('deleting', id) }}
+            onSaveEdit={(id, newValue) => { console.log(id, newValue) }}
+          />
+          <div className="row no-gutters">
+            <div className="col">
+              <BottomBtn 
+                text="新建"
+                colorClass="btn-primary"
+                icon={faPlus}
+              />
+            </div>
+            <div className="col">
+              <BottomBtn 
+                text="导入"
+                colorClass="btn-success"
+                icon={faFileImport}
+              />
+            </div>
+          </div>
         </div>
         <div className="col bg-primary right-panel">
           <h1>this is the right</h1>
--- cloud-doc/src/components/FileSearch.js
+++ cloud-doc/src/components/FileSearch.js
@@ -2,27 +2,24 @@ import React, { useState, useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch, faTimes } from '@fortawesome/free-solid-svg-icons'
+import useKeyPress from '../hooks/useKeyPress'
 
 const FileSearch = ({ title, onFileSearch }) => {
   const [inputActive, setInputActive] = useState(false)
   const [value, setValue] = useState('')
+  const enterPressed = useKeyPress(13)
+  const escPressed = useKeyPress(27)
   let node = useRef(null)
-  const closeSearch = (e) => {
-    e.preventDefault()
+  const closeSearch = () => {
     setInputActive(false)
+    setValue('')
   }
   useEffect(() => {
-    const handleInputEvent = (event) => {
-      const { keyCode } = event
-      if (keyCode === 13 && inputActive) {
-        onFileSearch(value)
-      } else if (keyCode === 27 && inputActive) {
-        closeSearch(event)
-      }
+    if (enterPressed && inputActive) {
+      onFileSearch(value)
     }
-    document.addEventListener('keyup', handleInputEvent)
-    return () => {
-      document.removeEventListener('keyup', handleInputEvent)
+    if (escPressed && inputActive) {
+      closeSearch()
     }
   }) 
   useEffect(() => {
@@ -31,7 +28,7 @@ const FileSearch = ({ title, onFileSearch }) => {
     }
   }, [inputActive])
   return (
-    <div className="alert alert-primary d-flex justify-content-between align-items-center">
+    <div className="alert alert-primary d-flex justify-content-between align-items-center mb-0">
       {
         !inputActive &&
         <>
