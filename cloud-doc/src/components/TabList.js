651a8aab6de913f61ad32e4658fe3de89e08bfee
kcshan <cshan.kong@gmail.com>
2021-01-05 08:50:13 +0800

    fix

--- cloud-doc/src/components/TabList.js
+++ cloud-doc/src/components/TabList.js
@@ -1,21 +1,34 @@
 import React, { useState, useEffect } from 'react'
+import classNames from 'classnames'
 import PropTypes from 'prop-types'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
 
 const TabList = ({ files, activeId, unsaveIds, onTabClick, onCloseTab }) => {
   return (
     <ul className="nav nav-pills">
       {
         files.map(file => {
+          const fClassName = classNames({
+            'nav-link': true,
+            'active': file.id === activeId
+          })
           return (
             <li
               className="nav-item"
               key={file.id}
             >
               <a 
-                className="nav-link" 
+                className={fClassName} 
                 href="#"
+                onClick={(e) => {e.preventDefault();onTabClick(file.id)}}
               >
                 {file.title}
+                <span className="ml-2">
+                  <FontAwesomeIcon
+                    icon={faTimes} 
+                  />
+                </span>
               </a>
             </li>
           )
